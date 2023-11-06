7dbd0ae69dc516f9c372644de4ccd6c678a84610
naeemsultan <naeemsultan2020@gmail.com>
2023-11-06 10:52:57 +0300

    app.py updated

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
+app = Flask(__name__)
+
 
 @app.route("/")
 def home():
     return "Hello Python with Flask"
 
-
-app = Flask(__name__)
 
 if __name__ == "__main__":
     app.run(debug=True)
