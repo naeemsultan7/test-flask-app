7d921b942fff5cdb49a5ec3950c3510bc478b882
naeemsultan <naeemsultan2020@gmail.com>
2023-11-06 13:38:37 +0300

    files updated

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "Hello Python with Flask"
+    name = input("Please type your name: ")
+
+    return render_template("home.html", data={'name': name})
+
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    if request.method == "POST":
+        num1 = int(request.form['num1'])
+        num2 = int(request.form['num2'])
+        result = num1 + num2
+
+        return render_template("about.html", result={'result': result})
+    else:
+        return render_template('about.html', result={'result': 'none'})
 
 
 if __name__ == "__main__":
